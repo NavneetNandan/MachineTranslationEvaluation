94c3c0f4af0c89a706850f32acf749a672836607
NavneetNandan <navneet.nandan8@gmail.com>
2017-06-22 11:30:18 +0530

    added lbleu corpus wise evaluation metric

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 import sys
-
+from pythonrouge.pythonrouge import Pythonrouge
 from nltk import RegexpTokenizer
 import lebleu.lebleu.lebleu as lb
 
@@ -21,6 +21,8 @@ if __name__ == '__main__':
         reference_file_path = sys.argv[2]
         mode = sys.argv[3]
         tokenizer = RegexpTokenizer(r'\w+')
+        ROUGE_path = "/home/navneet/pythonrouge/pythonrouge/RELEASE-1.5.5/ROUGE-1.5.5.pl" # ROUGE-1.5.5.pl
+        data_path = "/home/navneet/pythonrouge/pythonrouge/RELEASE-1.5.5/data"  # data folder in RELEASE-1.5.5
         leblue_scorer = lb.LeBLEU()
         if os.path.exists(hypothesis_file_path) and os.path.exists(reference_file_path):
             if mode in ["-c", "-s"]:
@@ -30,25 +32,30 @@ if __name__ == '__main__':
                                                 for hypothesis_sentence in hypothesis_file.readlines()]
                         reference_sentences = [reference_sentence.rstrip()
                                                for reference_sentence in reference_file.readlines()]
-                        with open("hyp.temp", "w") as temp_hyp:
-                            for i, hypothesis_sentence in enumerate(hypothesis_sentences):
-                                temp_hyp.write('{0} ({1})\n'.format(hypothesis_sentence, str(i)))
-                        with open("ref.temp", "w") as temp_ref:
-                            for i, reference_sentence in enumerate(reference_sentences):
-                                temp_ref.write('{0} ({1})\n'.format(reference_sentence, str(i)))
-                        subprocess.run(
-                            "java -jar tercom-0.7.25/tercom.7.25.jar -r ref.temp -h hyp.temp -n out -o sum",
-                            stdout=subprocess.PIPE, shell=True)
-                        with open("out.sum", "r") as eval_out:
-                            out_sum_lines = eval_out.readlines()[5:len(reference_sentences) + 5]
-                        meteor_evaluation_output = subprocess.run(
-                            "java -Xmx2G -jar ~/meteor_test/meteor-*.jar {0} {1} -l en -norm".format(
-                                hypothesis_file_path,reference_file_path),
-                            stdout=subprocess.PIPE, shell=True)
-                        meteor_evaluation_output = meteor_evaluation_output.stdout.decode("UTF-8")
-                        meteor_evaluation_sentence_wise = meteor_evaluation_output.split('\n')[
-                                                          11:11 + len(reference_sentences)]
                         if len(reference_sentences) == len(hypothesis_sentences):
+                            with open("hyp.temp", "w") as temp_hyp:
+                                for i, hypothesis_sentence in enumerate(hypothesis_sentences):
+                                    temp_hyp.write('{0} ({1})\n'.format(hypothesis_sentence, str(i)))
+                            with open("ref.temp", "w") as temp_ref:
+                                for i, reference_sentence in enumerate(reference_sentences):
+                                    temp_ref.write('{0} ({1})\n'.format(reference_sentence, str(i)))
+                            subprocess.run(
+                                "java -jar tercom-0.7.25/tercom.7.25.jar -r ref.temp -h hyp.temp -n out -o sum",
+                                stdout=subprocess.PIPE, shell=True)
+                            with open("out.sum", "r") as eval_out:
+                                out_sum_lines = eval_out.readlines()[5:len(reference_sentences) + 5]
+                            meteor_evaluation_output = subprocess.run(
+                                "java -Xmx2G -jar ~/meteor_test/meteor-*.jar {0} {1} -l en -norm".format(
+                                    hypothesis_file_path, reference_file_path),
+                                stdout=subprocess.PIPE, shell=True)
+                            meteor_evaluation_output = meteor_evaluation_output.stdout.decode("UTF-8")
+                            meteor_evaluation_sentence_wise = meteor_evaluation_output.split('\n')[
+                                                              11:11 + len(reference_sentences)]
+
+                            # rouge = Pythonrouge(n_gram=2, ROUGE_SU4=True, ROUGE_L=True, stemming=True, stopwords=True,
+                            #                     word_level=True, length_limit=True, length=50, use_cf=False, cf=95,
+                            #                     scoring_formula="average", resampling=True, samples=1000, favor=True,
+                            #                     p=0.5)
                             if mode == '-s':
                                 evaluation_list = list(range(0, len(hypothesis_sentences)))
                                 count = 0
@@ -73,7 +80,15 @@ if __name__ == '__main__':
                                                                 / 100
                                     evaluation_list[i]['meteor'] = float(
                                         meteor_evaluation_sentence_wise[i].split(":")[1].rstrip().lstrip())
+                                    # setting_file = rouge.setting(files=False, summary=hypothesis_sentence,
+                                    #                              reference=reference_sentence)
+                                    # result = rouge.eval_rouge(setting_file, recall_only=True, ROUGE_path=ROUGE_path,
+                                    #                           data_path=data_path)
+                                    # print(result)
                                 print(evaluation_list)
+                            if mode == '-c':
+                                lbleu_score = leblue_scorer.eval(hypothesis_sentences,reference_sentences)
+                                print(lbleu_score)
                         else:
                             print("""Number of sentences in hypothesis file and reference file is not equal""")
                             sys.exit(1)
